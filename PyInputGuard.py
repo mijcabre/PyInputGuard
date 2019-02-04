b399006c8120175cb62eaa00845810503e213e4d
mijcabre <mijcabre@gmail.com>
2019-02-04 13:50:20 -0600

    Added length/value checking

--- PyInputGuard.py
+++ PyInputGuard.py
@@ -1,40 +1,88 @@
-# TODO: add checking data directly(not using a prompt, example: x = enforceInt(someData))
-# TODO: add length/size checking to enforceInt, enforceFloat, enforceString, ...?
 # TODO: add precision checking to enforceFloat
+# TODO: add hardcore enforcement functions (loops until user gives valid input)
+# TODO: DRY/refactor code
+# TODO: add check to make sure maxValue > minValue && maxLength > minLength
 # TODO: add documentation for methods
 # TODO: add documentation in README
-# TODO: add hardcore enforcement functions (loops until user gives valid input)
-# TODO: add check for negative or positive numbers
-
 
 import re
 
-def enforceInt(prompt):
+def enforceInt(prompt, minValue = None, maxValue = None):
     testInput = input(prompt)
     try:
         testInput = int(testInput)
-        return testInput
+        if minValue and maxValue:
+            if testInput >= minValue and testInput <= maxValue:
+                return testInput
+            else:
+                return f'Input "{testInput}" must be between {minValue - 1} and {maxValue + 1}.'
+        elif minValue:
+            if testInput >= minValue:
+                return testInput
+            else:
+                return f'Input "{testInput}" is too small. Input must be at least {minValue}.'
+        elif maxValue:
+            if testInput <= maxValue:
+                return testInput
+            else:
+                return f'Input "{testInput}" is too large. Input must be less than {maxValue + 1}.'
+        else:
+            return testInput
     except:
         return f'Input "{testInput}" cannot be converted into an integer.'
 
-def enforceFloat(prompt):
+def enforceFloat(prompt, minValue = None, maxValue = None):
     testInput = input(prompt)
     try:
         testInput = float(testInput)
+        if minValue and maxValue:
+            if testInput >= minValue and testInput <= maxValue:
+                return testInput
+            else:
+                return f'Input "{testInput}" must be between {minValue - 1} and {maxValue + 1}.'
+        elif minValue:
+            if testInput >= minValue:
+                return testInput
+            else:
+                return f'Input "{testInput}" is too small. Input must be at least {minValue}.'
+        elif maxValue:
+            if testInput <= maxValue:
+                return testInput
+            else:
+                return f'Input "{testInput}" is too large. Input must be less than {maxValue + 1}.'
+        else:
+            return testInput
         return testInput
     except:
         return f'Input "{testInput}" cannot be converted into a float.'
 
-def enforceStringFormat(prompt, regex = None):
+def enforceStringFormat(prompt,  regex = None, minLength = None, maxLength = None):
     testInput = input(prompt)
     if regex != None:
         pattern = re.compile(regex)
         if bool(pattern.match(testInput)):
             return testInput
         else:
-            return f'Input "{testInput}" does not fit the specified format.'
+           return f'Input "{testInput}" does not fit the specified format.'
     else:
-        return testInput
+        testInputLength = len(testInput)
+        if minLength and maxLength:
+            if testInputLength >= minLength and testInputLength <= maxLength:
+                return testInput
+            else:
+                return f'Input "{testInput}" must be between {minLength - 1} and {maxLength + 1} characters long.'
+        elif minLength:
+            if testInputLength >= minLength:
+                return testInput
+            else:
+                return f'Input "{testInput}" is too small. Input must be at least {minLength} characters long.'
+        elif maxLength:
+            if testInputLength <= maxLength:
+                return testInput
+            else:
+                return f'Input "{testInput}" is too large. Input must be less than {maxLength + 1} characters long.'
+        else:
+            return testInput
 
 def enforceBool(prompt):
     testInput = input(prompt)
